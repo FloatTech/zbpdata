f2fa48b3d7112c8a55c6d8f621d98aee53d41166
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-05-04 20:26:05 +0800

    fix: push

--- main.go
+++ main.go
@@ -10,11 +10,9 @@ import (
 	"io/fs"
 	"os"
 	"strings"
-	"sync"
 	"unicode"
 	"unsafe"
 
-	"github.com/FloatTech/zbputils/process"
 	"github.com/fumiama/go-registry"
 	"github.com/wdvxdr1123/ZeroBot/utils/helper"
 )
@@ -46,26 +44,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer r.Close()
-	var wg sync.WaitGroup
-	wg.Add(len(files))
 	for i, fn := range files {
-		go func(i int, fn string) {
-			defer wg.Done()
-			for c := 0; c < 5; c++ {
-				err = r.Set("data/"+fn, md5s[i])
-				fmt.Println("set", "data/"+fn, "=", hex.EncodeToString(helper.StringToBytes(md5s[i])))
-				if err == nil {
-					break
-				}
-				if c >= 4 {
-					panic("ERROR:" + err.Error() + "max retry times exceeded")
-				} else {
-					fmt.Println("ERROR:", err, ", retry times:", c)
-				}
-				process.SleepAbout1sTo2s()
+		for c := 0; c < 5; c++ {
+			err = r.Set("data/"+fn, md5s[i])
+			fmt.Println("set", "data/"+fn, "=", hex.EncodeToString(helper.StringToBytes(md5s[i])))
+			if err == nil {
+				break
 			}
-		}(i, fn)
+			if c >= 4 {
+				panic("ERROR:" + err.Error() + "max retry times exceeded")
+			} else {
+				fmt.Println("ERROR:", err, ", retry times:", c)
+			}
+			_ = r.Close()
+			err = r.Connect()
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
-	wg.Wait()
+	_ = r.Close()
 }
